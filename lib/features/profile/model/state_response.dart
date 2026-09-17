ac2b11544bb98fac35868a1891d4edee17dd9855
Kareena-Singh <kareenasingh875@gmail.com>
2026-09-17 11:38:24 +0530

    Profile API Created

--- lib/features/profile/model/state_response.dart
+++ lib/features/profile/model/state_response.dart
@@ -1,6 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'state_response.freezed.dart';
+
 part 'state_response.g.dart';
 
 @freezed
@@ -18,10 +19,8 @@ class StateResponse with _$StateResponse {
 
 @freezed
 class StateModel with _$StateModel {
-  const factory StateModel({
-    required int id,
-    required String name,
-  }) = _StateModel;
+  const factory StateModel({required int id, required String name}) =
+      _StateModel;
 
   factory StateModel.fromJson(Map<String, dynamic> json) =>
       _$StateModelFromJson(json);
